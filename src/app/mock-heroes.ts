d9f03463405cd43317dec7af2a59f5a99c42596e
Denis Chudnov <denis.chudnov@saritasa.com>
2021-07-19 18:39:11 +0700

    initial commit - feature component created, startof working with services

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Homelander' },
+  { id: 12, name: 'Queen Mayve' },
+  { id: 13, name: 'Black Noir' },
+  { id: 14, name: 'Invisible' },
+  { id: 15, name: 'A-Train' },
+  { id: 16, name: 'Lighter man' },
+  { id: 17, name: 'Starlight' },
+  { id: 18, name: 'Stormfront' },
+  { id: 19, name: 'Solder boy' },
+  { id: 20, name: 'Red wing' }
+];
